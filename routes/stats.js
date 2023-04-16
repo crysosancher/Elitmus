7763a84322bf8f420b80bedeb70b3e4aa8bc2d4e
crysosancher <vibhupandey.vp@gmail.com>
2023-04-17 04:00:51 +0530

    done without admin

--- routes/stats.js
+++ routes/stats.js
@@ -59,6 +59,122 @@ async function insert1(email, score, time) {
     console.log(err.stack);
   }
 }
+async function insert2(email,score){
+  try {
+    await client.connect();
+    console.log("Connected correctly to Mongodb");
+
+    const user = await client
+      .db("elitmus")
+      .collection("stats")
+      .findOne({ email: email });
+    console.log(user);
+    if (!user) {
+      //Entering new records
+      console.log("user not found");
+      await client
+        .db("elitmus")
+        .collection("stats")
+        .insertOne({
+          email: email,
+          score: { game2: [score] },
+          time: { game2: ['0'] },
+        })
+        .then(() => {
+          console.log("inserted");
+        });
+    } else {
+      //Updating existing records
+      console.log("user found");
+      await client
+        .db("elitmus")
+        .collection("stats")
+        .updateOne(
+          { email: email },
+          {
+            $push: {
+              [`score.game2`]: score,
+              [`time.game2`]: '0',
+            },
+          }
+        )
+        .then(() => {
+          console.log("inserted");
+        });
+    }
+  } catch (err) {
+    console.log(err.stack);
+  }
+}
+async function insert3(email,score){
+  try {
+    await client.connect();
+    console.log("Connected correctly to Mongodb");
+
+    const user = await client
+      .db("elitmus")
+      .collection("stats")
+      .findOne({ email: email });
+    console.log(user);
+    if (!user) {
+      //Entering new records
+      console.log("user not found");
+      await client
+        .db("elitmus")
+        .collection("stats")
+        .insertOne({
+          email: email,
+          score: { game3: [score] },
+          time: { game3: ['0'] },
+        })
+        .then(() => {
+          console.log("inserted");
+        });
+    } else {
+      //Updating existing records
+      console.log("user found");
+      await client
+        .db("elitmus")
+        .collection("stats")
+        .updateOne(
+          { email: email },
+          {
+            $push: {
+              [`score.game3`]: score,
+              [`time.game3`]: '0',
+            },
+          }
+        )
+        .then(() => {
+          console.log("inserted");
+        });
+    }
+  } catch (err) {
+    console.log(err.stack);
+  }
+}
+
+statsRouter.post("/3", function (req, res, next) {
+  console.log("inside insert");
+  if (req.session.loggedIn) {
+    console.log(req.body);
+    insert3(req.session.email, req.body.score);
+    res.send("inserted");
+  } else {
+    res.redirect("/");
+  }
+})
+statsRouter.post("/2", function (req, res, next) {
+  console.log("inside insert");
+  if (req.session.loggedIn) {
+    console.log(req.body);
+    insert2(req.session.email, req.body.score);
+    res.send("inserted");
+  } else {
+    res.redirect("/");
+  }
+});
+
 statsRouter.post("/1", function (req, res, next) {
   console.log("inside insert");
   if (req.session.loggedIn) {
